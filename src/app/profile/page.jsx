f947599cbf8a336df58a00396ffd6ca72951bb6a
saideeWD <mdsaideehasan@gmail.com>
2025-01-24 20:04:40 +0600

    projects costomize

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Profile() {
   return (
     <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae totam molestias labore ullam. Dicta animi commodi mollitia, sunt deleniti ducimus atque nobis impedit eius consectetur. Numquam est nostrum fuga recusandae!
+      uae totam molestias labore ullam. Dicta animi commodi mollitia, sunt deleniti ducimus atque nobis impedit eius consectetur. Numquam est nostrum fuga recusandae!
     </div>
   )
 }
